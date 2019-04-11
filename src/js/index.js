9cc8593e2aa354445d0d8d022b4ea5ad4098a176
max <maxsheng101@gmail.com>
2019-04-11 18:56:35 -0400

    form and stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 App = {
     web3Provider: null,
     contracts: {},
+    contract_addr: "0x0",
     account: '0x0',
     hasVoted: false,
   
@@ -21,6 +22,11 @@ App = {
         web3 = new Web3(App.web3Provider);
       }
       App.account = web3.eth.accounts[0];
+      if ($('#my_account') != null){
+        $('#my_account').append("<p>" +"my address is "+ App.account + "</p>");
+      }
+      
+
       return App.initContract();
     },
 
@@ -32,36 +38,44 @@ App = {
           // Connect provider to interact with contract
           App.contracts.ScoreContract.setProvider(App.web3Provider);
     
-          //App.listenForEvents();
-    
-          //return App.addInst();
-          //console.log(App.contracts.ScoreContract)
+          //read contract address
+          App.contracts.ScoreContract.deployed().then(function(instance) {
+            //console.log("call function");
+            console.log("contract address: " + App.contracts.ScoreContract.address);
+            App.contract_addr = App.contracts.ScoreContract.address;
+            //return instance.addInstitution(addr, name, { from: App.account });
+            return App.checkInst();
+        })
+          
         });
-        //return
+        
       },
 
-      addInst: function(addr, name){
-        App.account = web3.eth.accounts[0];
-  
+      addInst: function(){
+        //var stu_addr = $('.get_record_form').find("input.stu_addr").val();
+        var inst_addr = $('.add_inst_form').find("input.inst_addr").val();
+        var inst_name = $('.add_inst_form').find("input.inst_name").val();
+        console.log(App.account);
+        console.log("adding: "+ typeof(inst_name) + " " + typeof(inst_addr));
+
         App.contracts.ScoreContract.deployed().then(function(instance) {
             //console.log("call function");
-            return instance.addInstitution(addr, name, { from: App.account });
+            return instance.addInstitution(inst_addr, inst_name, { from: App.account });
         })
       },
 
       getInst: function(fn){
-            
+            //console.log(App.contract_addr);
             const filter = web3.eth.filter({
                 fromBlock:0,
                 toBlock: 'latest',
-                address: '0xF851C83cC0A723cE9427e79B65350eaaf0A38451', // contract address
+                address: App.contract_addr, // contract address
                 topics: [web3.sha3("addInstitutionEvent(address)")]
             })
             var result = filter.get(function(error, log){
                 insts = []
-                //console.log(error);
+                //console.log(log);
                 for (var i=0; i<log.length; i++){
-                    //console.log(one_log.data);
                     insts.push(log[i].data);
                 }
                 fn(insts);
@@ -81,9 +95,9 @@ App = {
               for (var i=0; i<insts.length; i++){
               //console.log((App.account))
                 var inst_account = parseInt(insts[i])
-                var my_account = parseInt(App.account)
-                console.log(my_account)
-                console.log(inst_account)
+                var my_account = parseInt(web3.eth.accounts[0])
+                console.log("my_account: " + my_account)
+                console.log("isnt_account: " + inst_account)
                 if (my_account == inst_account){
                   $('#my_account').append('<p>' + 'I am an institution' + '</p>');
                   a_tag.setAttribute("href", "./institution.html");
@@ -106,19 +120,30 @@ App = {
       },
 
       addRecord: function(){
-           var students_json = 
-          {
-              // "yilin" : {"address": "0xAEBC5e957A949802b0c0E4503BD333b3da95912C", "english": 4.0, "math" : 3.8, "physics":3.0}
-              "ma" : {"address": "0xe77b9bA35c792450eF94D9D5f53399A88F0c8c01", "english": 4.0, "math" : 3.8, "physics":3.0}
-          }
+          //  var students_json = 
+          // {
+          //     // "yilin" : {"address": "0xAEBC5e957A949802b0c0E4503BD333b3da95912C", "english": 4.0, "math" : 3.8, "physics":3.0}
+          //     "ma" : {"address": "0xe77b9bA35c792450eF94D9D5f53399A88F0c8c01", "english": 4.0, "math" : 3.8, "physics":3.0}
+          // }
+
+          var stu_name = $('.add_record_form').find("input.stu_name").val()
+          var stu_addr = $('.add_record_form').find("input.stu_addr").val()
+          var student_json = {}
+          var score = {}
+          score["address"] = stu_addr
+          score[$('.add_record_form').find("input.subject_1").val()] = $('.add_record_form').find("input.score_1").val()
+          score[$('.add_record_form').find("input.subject_2").val()] = $('.add_record_form').find("input.score_2").val()
+          score[$('.add_record_form').find("input.subject_3").val()] = $('.add_record_form').find("input.score_3").val()
+          student_json[stu_name] = score;
+          
+          console.log(student_json)
 
            var hashedRecord = web3.sha3(JSON.stringify(students_json));
-           console.log(hashedRecord);
-           console.log(JSON.stringify(students_json));
+           console.log("record hash: " + hashedRecord);
            App.contracts.ScoreContract.deployed().then(function(instance) {
             //console.log("call function");
             //return instance.postScoreRecord(students_json["yilin"]["address"], hashedRecord, { from: App.account });
-            return instance.postScoreRecord(students_json["ma"]["address"], hashedRecord, { from: App.account });
+            return instance.postScoreRecord(stu_addr, hashedRecord, { from: App.account });
           })
            // add record to database
            $.ajax({
@@ -162,15 +187,15 @@ App = {
               }
            })
           })
-
-
       }, 
 
       requestAccess: function(){
+           var stu_addr = $('.request_access_form').find("input.stu_addr").val();
+           console.log("requesting: "+ stu_addr)
            App.contracts.ScoreContract.deployed().then(function(instance) {
             //console.log("call function");
             //return instance.getScoreRecord("0xAEBC5e957A949802b0c0E4503BD333b3da95912C", App.account, { from: App.account });
-            return instance.postAccessRequest("0xe77b9bA35c792450eF94D9D5f53399A88F0c8c01", { from: App.account });
+            return instance.postAccessRequest(stu_addr, { from: App.account });
           })
       },
 
@@ -182,7 +207,7 @@ App = {
               const filter = web3.eth.filter({
                 fromBlock:0,
                 toBlock: 'latest',
-                address: '0xF851C83cC0A723cE9427e79B65350eaaf0A38451', // contract address
+                address: App.contract_addr, // contract address
                 topics: [web3.sha3("accessRequestEvent(address,address)")]
               });
               var result = filter.get(function(error, log){
@@ -190,7 +215,7 @@ App = {
                 const filter2 = web3.eth.filter({
                   fromBlock:0,
                   toBlock: 'latest',
-                  address: '0xF851C83cC0A723cE9427e79B65350eaaf0A38451', // contract address
+                  address: App.contract_addr, // contract address
                   topics: [web3.sha3("accessControllEvent(address,address,bool)")]
                 });
 
@@ -267,8 +292,8 @@ App = {
 $(document).ready(function(){
     App.init();
 
-    $('#my_account').append("<p>" +"my address is "+ App.account + "</p>");
-    App.checkInst();
+    //$('#my_account').append("<p>" +"my address is "+ App.account + "</p>");
+    //App.checkInst();
     //App.getInst();
 })
 
